6cce499467f325d562773294fe205cc6ce6e0618
arbor <arbor@o2.pl>
2016-07-10 20:43:57 +0200

    vcv

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -6,7 +6,7 @@ import { InputContainer, PanelContainer, MainMenuContainer } from 'ui';
 dom.render(
     (
         <div style={{ margin: '0 auto', width: '800px' }}>
-            <MainMenuContainer menuItems={['dsadas', 'dsa']} />
+            <MainMenuContainer menuItems={['dsadas', 'dsa3', 'dsa4', 'dsa5', 'dsa6', 'dsa7', 'dsa8', 'dsa9', 'dsa66', 'dsa45', 'dsa66', 'dsa654']} />
             <PanelContainer>
                 <InputContainer value={'test'} />
             </PanelContainer>
--- frontend/ui/user-interface/mainMenu/index.tsx
+++ frontend/ui/user-interface/mainMenu/index.tsx
@@ -10,13 +10,18 @@ let menuContainerStyle = {
 
 export default class MainMenuComponent extends React.Component<UserInterface.MainMenu.IProps, UserInterface.MainMenu.IState> {
     public componentWillMount() {
-        this.state = { isOpen: false, menuItems: [] };
+        this.state = { isOpen: false, menuItems: this.props.menuItems };
+    }
+    public componentWillReceiveProps(nextProps: { menuItems: Array<string> }) {
+        this.setState(Object.assign({}, this.state, { menuItems: nextProps.menuItems }));
     }
     public render(): React.ReactElement<{}> {
+        let itemStyle = { 'lineHeight' : '50px', 'textAlign' : 'center', 'borderBottom' : '1px solid #ececec', 'display' : 'block' };
+        let items = this.state.menuItems.map(i => <a href={i} key={i} style={itemStyle}>{i}</a>);
         return (
             <div>
                 <button onClick={() => { this.setState(Object.assign({}, this.state, { isOpen: true })) } } className='mui-btn mui-btn--fab mui-btn--primary' style={menuContainerStyle}>+</button>
-                <Asside isOpen={this.state.isOpen}><div>content</div></Asside>
+                <Asside isOpen={this.state.isOpen}><div>{items}</div></Asside>
             </div>
         );
     }
