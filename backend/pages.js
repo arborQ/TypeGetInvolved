8515b87557b67587ebb5a4b7b464182f42df2b71
arbor <arbor@o2.pl>
2016-09-22 00:00:17 +0200

    Revert "try render"
    This reverts commit d8a912354a0e9d2f79e2a53e1b23a439dd04ae12.

--- backend/pages.js
+++ backend/pages.js
@@ -1,12 +1,6 @@
 import express from'express';
 import { enviroment, application } from '../config/setup';
 
-import * as React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-
-import routes from '../frontend/routes.tsx';
-
 var router = express.Router();
 
 /* GET home page. */
@@ -14,11 +8,6 @@ router.get('*', function(req, res, next) {
     if(req.originalUrl.indexOf('/api/') === 0){
       return next('route');
     }else{
-      match({ routes, location: req.url },
-      (err, redirectLocation, renderProps) => {
-        console.log(renderProps);
-      });
-
       return res.render('index', { title : application, development : enviroment !== 'production' });
     }
 });
--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import * as dom from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-import routes from './routes';
+import MasterPage from './components/master';
+import NoMatch from './components/noMatch';
+
+import { LoginPage } from './components/authorize';
 
 dom.render(
     (
         <div>
-            <Router history={browserHistory} routes={routes} onUpdate={() => window.scrollTo(0, 0)}> </Router>
+            <Router history={browserHistory}>
+                <Route path='/' component={MasterPage}>
+                    <Route path='login' component={LoginPage} />
+                    <Route path='*' component={NoMatch}/>
+                </Route>
+            </Router>
         </div>
     ), document.getElementById('app'));
