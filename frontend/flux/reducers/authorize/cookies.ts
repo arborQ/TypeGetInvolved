1d7275f6c59ab5cabeecc1dcbee10cdfc2f99fad
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2016-09-21 13:05:05 +0200

    cookie

--- frontend/flux/reducers/authorize/cookies.ts
+++ frontend/flux/reducers/authorize/cookies.ts
@@ -1,11 +1,11 @@
-export function getCookie(name : string) : string {
-  var value = "; " + document.cookie;
-  var parts = value.split(`; ${name}=`);
-  if (parts.length == 2) {
-    return parts.pop().split(";").shift();
+export function getCookie(name: string): string {
+  let value: string = '; ' + document.cookie;
+  let parts: string[] = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
   }
 };
 
-export function deleteCookie( name : string ) : void {
+export function deleteCookie(name: string ): void {
   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
