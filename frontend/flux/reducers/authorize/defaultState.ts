8ec35e4c948b30cb771f86dff9689d36415ab0b4
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2016-09-21 12:54:04 +0200

    defaultState.ts

--- frontend/flux/reducers/authorize/defaultState.ts
+++ frontend/flux/reducers/authorize/defaultState.ts
@@ -1,18 +1,17 @@
 import { getCookie } from './cookies';
-export default function() : flux.reducers.authorize.IState {
-    var token = getCookie('jwt-token');
-    if(!!token){
-        return { 
+export default function(): flux.reducers.authorize.IState {
+    let token: string = getCookie('jwt-token');
+    if (!!token) {
+        return {
             Expire: new Date(),
-            IsAuthenticated : true,
-            UserName : token
+            IsAuthenticated: true,
+            UserName: token,
         };
-    }else{
-    return { 
+    } else {
+    return {
             Expire: new Date(),
             IsAuthenticated : false,
-            UserName : token
+            UserName : token,
         };
     }
-    
 }
