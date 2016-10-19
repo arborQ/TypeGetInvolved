319cce7e1574066b14b2ed5f013ab7a31d326c25
arborQ <arbor@o2.pl>
2016-10-19 07:57:35 +0200

    build (#5)
    * nice
    
    * refresh delete
    
    * 765756756
    
    * iyu
    
    * save edit
    
    * jghjghj
    
    * select options
    
    * dasdasdas
    
    * dsada
    
    * build

--- frontend/flux/reducers/messages/reducers/userCreated.ts
+++ frontend/flux/reducers/messages/reducers/userCreated.ts
@@ -1,6 +1,6 @@
 let reducer: store.messages.MessageReducer =  (state: any, action: any): any => {
     if (action.type === 'CREATE_USER_FULFILLED') {
-        return Object.assign({}, state, { Messages: [...this.state, { date: new Date(), text: 'New user added' }] });
+        return Object.assign({}, state, { Messages: [...this.state, { date: new Date(), text: 'New user added!' }] });
     }
 };
 
